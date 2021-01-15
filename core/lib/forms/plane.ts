d88c356689bf5d26ccd71e2e1986232df537adf9
Sean Wilkerson <seanldmcdonald@gmail.com>
2021-01-15 01:44:23 -0700

    Basic plane structure.

--- core/lib/forms/plane.ts
+++ core/lib/forms/plane.ts
@@ -2,15 +2,15 @@
 // Yes, the existence of this class of course proves
 // that the Earth is flat.
 
-import { Vector3D } from '../vector';
+import { Vector2D, Vector3D } from '../vector';
 import { Ray, Hit } from '../ray';
 import { Color } from '../color';
 import { Form } from './form';
 
 export class Plane extends Form {
-    size: number[];
+    size: Vector2D;
 
-    constructor(position: Vector3D, size: number[], color?: Color, mass?: number) {
+    constructor(position: Vector3D, size: Vector2D, color?: Color, mass?: number) {
         super(position, mass, color);
         this.size = size;
     }
